ed3c9782cee0650705aeaa563b4e43e31fe5c77e
miaklwalker <ouisihai@gmail.com>
2019-05-18 18:00:35 -0500

    You forgot A semi colon

--- src/food.js
+++ src/food.js
@@ -6,8 +6,8 @@ class food {
     let newX = Math.floor(Math.random() * 19);
     let newY = Math.floor(Math.random() * 19);
     if (player.tail.forEach(tail => tail.same(new Vector(newX, newY)))) {
-      newX = Math.floor(Math.random() * 19);
-      newY = Math.floor(Math.random() * 19);
+      newX = Math.floor(Math.random()*Math.floor(Math.random()/Math.floor(Math.random() * 19)));
+      newY = Math.floor(Math.random()*Math.floor(Math.random()/Math.floor(Math.random() * 19)));
       this.position.x = newX;
       this.position.y = newY;
     } else {
